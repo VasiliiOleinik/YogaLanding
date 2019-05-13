f1c9d8108382d3f26951ca11f85c2bebf136343f
HotBun <goooneoooogle@gmail.com>
2019-05-13 11:30:37 +0300

    Promise in form

--- js/script.js
+++ js/script.js
@@ -199,41 +199,76 @@ window.addEventListener("DOMContentLoaded", () => {
         event.preventDefault();
         form.appendChild(statusMessage);
 
-        let request = new XMLHttpRequest();
-        request.open('POST', 'server.php');
-           request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        //     let request = new XMLHttpRequest();
+        //     request.open('POST', 'server.php');
+        //        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
 
+        //     let formData = new FormData(form);
+
+        //     let obj = {};
+        //     formData.forEach(function (value, key) {
+        //         obj[key] = value;
+        //     });
+
+        //     let json = JSON.stringify(obj);
+
+        //     request.send(json);
+
+        //     request.addEventListener('readystatechange', () => {
+        //         if (request.readyState < 4) {
+        //             statusMessage.innerHTML = message.loading;
+        //         } else if (request.readyState === 4 && request.status == 200) {
+        //             statusMessage.innerHTML = message.success;
+        //         } else {
+        //             statusMessage.innerHTML = message.failure;
+        //         }
+        //     });
+
+        //     for (let i = 0; i < input.length; i++) {
+        //         input[i].value = '';
+        //     }
+
+        // 
+
+        // PROMISE
         let formData = new FormData(form);
 
-        let obj = {};
-        formData.forEach(function (value, key) {
-            obj[key] = value;
-        });
+        function postData(data) {
 
-        let json = JSON.stringify(obj);
+            return new Promise(function (resolve, reject) {
+                let request = new XMLHttpRequest();
+                request.open('POST', 'server.php');
+                request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
 
-        request.send(json);
+                request.onreadystatechange = function () {
+                    if (request.readyState < 4) {
+                        resolve()
+                    } else if (request.readyState === 4) {
+                        if (request.status == 200 && request.status < 3) {
+                            resolve()
+                        } else {
+                            reject()
+                        }
+                    }
+                }
+                request.send(data);
+            });
 
-        request.addEventListener('readystatechange', () => {
-            if (request.readyState < 4) {
-                statusMessage.innerHTML = message.loading;
-            } else if (request.readyState === 4 && request.status == 200) {
-                statusMessage.innerHTML = message.success;
-            } else {
-                statusMessage.innerHTML = message.failure;
-            }
-        });
-
-        for (let i = 0; i < input.length; i++) {
-            input[i].value = '';
         }
 
+        function clearInput() {
+            for (let i = 0; i < input.length; i++) {
+                input[i].value = '';
+            }
+        }
+
+        postData(formData)
+            .then(() => statusMessage.innerHTML = message.loading)
+            .then(() => {
+                statusMessage.innerHTML = '';
+            })
+            .catch(() => statusMessage.innerHTML = message.failure)
+            .then(clearInput)
     });
-
-
-
-
-
-
 
 });
